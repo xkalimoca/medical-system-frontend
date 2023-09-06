e82cf3e084c46d9a1ada605be554cdd031441778
xkalimoca <klmc_02@hotmail.com>
2023-09-05 19:22:38 -0500

    changes

--- middlewares/authMiddleware.js
+++ middlewares/authMiddleware.js
@@ -1,6 +1,25 @@
 const JWT = require('jsonwebtoken');
 
 module.exports = async(req,res,next)=> {
-    const token = req.headers.get('authorization').split(" ")[1]
+    try{
+        const token = req.headers.get('authorization').split(" ")[1]
+        JWT.verify(token,process.env.JWT_SECRET,(err, decode)=>{
+        if(err){
+            return res.status(200).send({
+              message: 'Auth Failed',
+              success:false
+            })
+        }else{
+            req.body.userId = decode.id
+            next()
+        }
+    })
+    }catch(error){
+       console.log(error)
+       res.status(500).send({
+            message: 'Auth Failed',
+            success:false
+       }) 
+    }
 
 }
