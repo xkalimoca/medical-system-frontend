60f0fce040f1fcf1b5066078cac61bed1e134a76
xkalimoca <klmc_02@hotmail.com>
2023-09-06 20:05:26 -0500

    update changes

--- client/src/App.js
+++ client/src/App.js
@@ -8,9 +8,9 @@ import ProtectedRoute from "./components/ProtectedRoute";
 import PublicRoute from "./components/PublicRoute";
 import ApplyDoctor from "./pages/ApplyDoctor";
 import NotificationPage from "./pages/NotificationPage";
-import Users from "./pages/admin/Users";
+import Users from "./pages/admin/User";
 import Doctors from "./pages/admin/Doctors";
-import Profile from "./pages/doctor/profile";
+import Profile from "./pages/doctor/ProfileDoctor";
 import BookingPage from "./pages/BookingPage";
 import Appointments from "./pages/Appointments";
 import DoctorAppointments from "./pages/doctor/DoctorAppointments";
@@ -48,7 +48,7 @@ function App() {
               }
             />
             <Route
-              path="/doctor/profile/:id"
+              path="/doctor/profiledoctor/:id"
               element={
                 <ProtectedRoute>
                   <Profile />
